e55910643f91eedefc78172fd15c62e160e03f04
theapexratio-dotcom <theapexratio@gmail.com>
2026-01-04 17:25:14 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Importing from your new folders
+// Importing from your specific folders
 import Hero from './components/Hero'; 
 import AboutCoach from './pages/AboutCoach'; 
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-import ClientDashboard from './pages/ClientDashboard';
 
 function App() {
   return (
@@ -23,7 +22,6 @@ function App() {
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/dashboard" element={<ClientDashboard />} />
       </Routes>
     </Router>
   );
