5b0364314484b815c47c8e26960cc5670c1518f3
theapexratio-dotcom <theapexratio@gmail.com>
2026-01-04 16:52:46 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// 1. Tell the app where the files are located
+// Importing from your new folders
 import Hero from './components/Hero'; 
-import AboutCoach from './AboutCoach'; 
+import AboutCoach from './pages/AboutCoach'; 
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import ClientDashboard from './pages/ClientDashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* 2. Create the web addresses (URLs) for each page */}
+        {/* Main Homepage */}
         <Route path="/" element={<Hero />} /> 
+        
+        {/* New Coach Page */}
         <Route path="/about" element={<AboutCoach />} />
+
+        {/* Other Pages */}
         <Route path="/landing" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<ClientDashboard />} />
       </Routes>
     </Router>
   );
