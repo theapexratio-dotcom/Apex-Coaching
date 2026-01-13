2fa0b4a08570a795d16530d018ca892158236502
theapexratio-dotcom <theapexratio@gmail.com>
2026-01-13 00:34:02 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Navbar from './components/Navbar';
 import AboutCoach from './pages/AboutCoach';
-import Biopage from './pages/biopage';
-import LandingPage from './pages/LandingPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Make AboutCoach the default home page */}
-        <Route path="/" element={<AboutCoach />} />
-
-        {/* About and Bio routes */}
-        <Route path="/about" element={<AboutCoach />} />
-        <Route path="/biopage" element={<Biopage />} />
-
-        {/* Other Pages */}
-        <Route path="/landing" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            {/* This line below makes the Coach page the default Home screen */}
+            <Route path="/" element={<AboutCoach />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<AboutCoach />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
